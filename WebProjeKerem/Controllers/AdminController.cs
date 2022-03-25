538cadff6b79e643cef0d227423533828dcb6f6d
Semih Dizdar <semih.dizdar@hitsoft.com.tr>
2022-03-25 20:02:13 +0300

    new commit

--- WebProjeKerem/Controllers/AdminController.cs
+++ WebProjeKerem/Controllers/AdminController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using System.Xml;
 using WebProjeKerem.Models;
+using WebProjeKerem.Models.Doviz;
 
 namespace WebProjeKerem.Controllers
 {
@@ -26,6 +30,46 @@ namespace WebProjeKerem.Controllers
         }
 
         public IActionResult GetUsers()
+        {
+            var list = db.Users.ToList();
+            return View(list);
+        }
+
+        public IActionResult GetCurrency()
+        {
+            XmlDocument xml = new XmlDocument(); // yeni bir XML dökümü oluşturuyoruz.
+            xml.Load("http://www.tcmb.gov.tr/kurlar/today.xml"); // bağlantı kuruyoruz.
+            var Tarih_Date_Nodes = xml.SelectSingleNode("//Tarih_Date"); // Count değerini olmak için ana boğumu seçiyoruz.
+            var CurrencyNodes = Tarih_Date_Nodes.SelectNodes("//Currency"); // ana boğum altındaki kur boğumunu seçiyoruz.
+            int CurrencyLength = CurrencyNodes.Count; // toplam kur boğumu sayısını elde ediyor ve for döngüsünde kullanıyoruz.
+
+            List<_Doviz> dovizler = new List<_Doviz>(); // Aşağıda oluşturduğum public class ile bir List oluşturuyoruz.
+            for (int i = 0; i < CurrencyLength; i++) // for u çalıştırıyoruz.
+            {
+                var cn = CurrencyNodes[i]; // kur boğumunu alıyoruz.
+                // Listeye kur bilgirini ekliyoruz.
+                dovizler.Add(new _Doviz
+                {
+                    Kod = cn.Attributes["Kod"].Value,
+                    CrossOrder = cn.Attributes["CrossOrder"].Value,
+                    CurrencyCode = cn.Attributes["CurrencyCode"].Value,
+                    Unit = cn.ChildNodes[0].InnerXml,
+                    Isim = cn.ChildNodes[1].InnerXml,
+                    CurrencyName = cn.ChildNodes[2].InnerXml,
+                    ForexBuying = cn.ChildNodes[3].InnerXml,
+                    ForexSelling = cn.ChildNodes[4].InnerXml,
+                    BanknoteBuying = cn.ChildNodes[5].InnerXml,
+                    BanknoteSelling = cn.ChildNodes[6].InnerXml,
+                    CrossRateOther = cn.ChildNodes[8].InnerXml,
+                    CrossRateUSD = cn.ChildNodes[7].InnerXml,
+                });
+            }
+
+            ViewData["dovizler"] = dovizler; // dovizler List değerini data ya atıyoruz ön tarafta viewbag ile çekeceğiz.
+            return View();
+        }
+
+        public IActionResult GetGoldPrices()
         {
             var list = db.Users.ToList();
             return View(list);
